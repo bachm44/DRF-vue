8c71b124d0eb8060e076e7cde515564d1e6a2c7c
bachm44 <bachm44@gmail.com>
2020-05-09 20:38:38 +0200

    Vuetify layout added

--- backend/frontend/src/views/Home.vue
+++ backend/frontend/src/views/Home.vue
@@ -1,24 +1,74 @@
 <template>
-  <v-container>
-    <Header />
-    <Todos />
-  </v-container>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      clipped
+    >
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-view-dashboard</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Dashboard</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-settings</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Settings</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      clipped-left
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Todo app</v-toolbar-title>
+    </v-app-bar>
+    <v-content>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <v-row
+          align="start"
+          justify="center"
+        >
+        <div class="text-xs-center">
+          <Todos />
+        </div>
+        </v-row>
+      </v-container>
+    </v-content>
+
+    <v-footer app>
+      <span>&copy; 2020</span>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-    import Todos from '../components/Todos.vue'
-    import Header from '../components/Header.vue'
-    import {APIService} from '../APIService'
-    
-    const apiService = new APIService();
-    
-    export default {
-        name: "Home",
-        components: {
+  import Todos from '../components/Todos.vue'
+  import Header from '../components/Header.vue'
+
+  export default {
+    name: "Home",
+    components: {
             Todos,
             Header,
-        },
-        methods: {
-        }
-    }
+    },
+    data: () => ({
+      drawer: null,
+    }),
+    created () {
+      this.$vuetify.theme.dark = true
+    },
+  }
 </script>
